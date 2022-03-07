98c27f6e0463deedcddbeb13f1d6a3ec2e48c94e
letuankiet212 <48205415+letuankiet212@users.noreply.github.com>
2022-03-07 17:32:03 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,61 @@
-import React from 'react'
+/* eslint-disable no-use-before-define */
+/* eslint-disable camelcase */
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
 
+interface Pokemons {
+  name: string
+  url: string
+}
+
+interface Pokemon {
+  id: number
+  name: string
+  sprites: {
+    front_default: string
+  }
+}
+
+const App: React.FC = () => {
+  const [pokemons, setPokemons] = useState<Pokemons[]>([])
+
+  const getPokemons = async () => {
+    await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1').then(res => {
+      if (res.status === 200) {
+        const { results } = res.data
+        results.forEach(async (pokemon: Pokemons[]) => {
+          const data = await axios.get(
+            `https://pokeapi.co/api/v2/pokemon/${pokemon.name}/`
+          )
+          console.log(data)
+        })
+      }
+    })
+  }
+
+  useEffect(() => {
+    getPokemons()
+  }, [])
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="pokemon-header">Pokemon</header>
+        <section className="collection-container">
+          {pokemons.map((pokemon, index) => {
+            return (
+              <div key={index}>
+                <p>Name:{pokemon.name}</p>
+                <img src={pokemon.url} alt="" />
+              </div>
+            )
+          })}
+        </section>
+      </div>
+    </div>
+  )
+}
+export default App
 // type Student = { name: string; age: number }
 
 // const student: Student = {
@@ -13,7 +69,11 @@ import React from 'react'
 //   name: 'Daniel',
 //   age: 21,
 // }
-function App() {
-  return <div className="App">hello 12</div>
-}
-export default App
+// Function
+// const printSomething: () => string = () => {
+//   const name = 'Tuaans Kietj'
+//   return name
+// }
+// function App() {
+//   return <div className="App">hello 12</div>
+// }
