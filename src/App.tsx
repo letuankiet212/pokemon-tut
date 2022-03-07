b22af0f297cbe40d470c5d52ffa2a90bab0ec023
letuankiet212 <nhanvien701@gmail.com>
2022-03-07 22:03:37 +0700

    Add React-Router
    Dùng Folder Page để chia trang , settup Router trang index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 /* eslint-disable no-use-before-define */
 import React from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
 
 function App() {
-  return <div className="text-red-400">hello</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
